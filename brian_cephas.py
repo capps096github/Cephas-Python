dc491142b5d62aba20d4d6727849fef857a38d12
C APPS GITHUB TEAM <84335028+capps096github@users.noreply.github.com>
2022-09-18 14:59:43 +0300

    Added ml

--- brian_cephas.py
+++ brian_cephas.py
@@ -1,4 +1,5 @@
 # %%
 print("\n---------------- - Assignment Operators - ----------------\n")
-
+# Firebase and Python
+# https://www.freecodecamp.org/news/how-to-get-started-with-firebase-using-python/
 # %%
